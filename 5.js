8c888f4248d23759da3531e15339e1b82a6c2dfa
sp <143427973+SuperUser1245@users.noreply.github.com>
2024-03-12 15:43:48 +0200

    Update 5.js

--- 5.js
+++ 5.js
@@ -17,4 +17,32 @@ class Parent {
       console.log('The appliance is turned off');
       this.pluggedIn = false;
     } else {
-      console.log('The ap
+      console.log('The appliance is already turned off');
+    }
+  }
+}
+
+class Lamp extends Parent {
+  constructor(wattage, name, pluggedIn) {
+    super(pluggedIn);
+    this.wattage = wattage;
+    this.name = name;
+  }
+}
+
+class Pc extends Parent {
+  constructor(wattage, name, pluggedIn) {
+    super(pluggedIn);
+    this.wattage = wattage;
+    this.name = name;
+  }
+}
+
+const DellLamp = new Lamp('6w', 'dell', true);
+const AsusPc = new Pc('60w', 'asus', false);
+
+console.log(DellLamp.pluggedIn);
+AsusPc.plugIn();
+DellLamp.unplug();
+console.log(DellLamp.pluggedIn);
+
